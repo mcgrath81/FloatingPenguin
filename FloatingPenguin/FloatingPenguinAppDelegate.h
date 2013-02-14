fb0829cbdd4f0f3a0f3cd80e418ee01c1a86eb0b
admin <admin@admins-MacBook-4.local>
2013-02-14 14:00:01 +0000

    Initial Commit

---
+++ FloatingPenguin/FloatingPenguinAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FloatingPenguinAppDelegate.h
+//  FloatingPenguin
+//
+//  Created by admin on 14/02/2013.
+//  Copyright (c) 2013 emuse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FloatingPenguinAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
